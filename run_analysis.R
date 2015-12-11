2b1f7824d7cec4f4df76363def5ff90a9934c95b
addinall <addinall@addinall.net>
2015-12-12 05:38:54 +1000

    Breakfast backup.  0530 time for eggs!

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,20 @@
 
-Merges the training and the test sets to create one data set.
-Extracts only the measurements on the mean and standard deviation for each measurement. 
-Uses descriptive activity names to name the activities in the data set
-Appropriately labels the data set with descriptive variable names. 
-From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+## CAPTAIN SLOG
+## vim: set expandtab tabstop=4 shiftwidth=4 autoindent smartindent:
+## File         : run_analysis.R
+## System       : Getting and Cleaning Data final exam
+## Date         : 09/08/2014
+## Author       : Mark Addinall
+## Synopsis     : This file is part of the course work
+##                assignments for the Johns Hopkins
+##                series of Data Science units.
+##                This unit is Getting and cleaning Data 
+##
+##              - Merges the training and the test sets to create one data set.
+##              - Extracts only the measurements on the mean and standard deviation for each measurement. 
+##              - Uses descriptive activity names to name the activities in the data set
+##              - Appropriately labels the data set with descriptive variable names. 
+##              From the data set in step 4, creates a second, independent tidy data set with the 
+##              average of each variable for each activity and each subject.
 
 
